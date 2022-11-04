f9f5e96423adff53264448e379c8f800ffd8ef30
James Herring <49248205+TheJamesHerring@users.noreply.github.com>
2022-11-04 10:50:11 -0400

    combine with another lab to learn external loads

--- Allfiles/Labs/15/setup.ps1
+++ Allfiles/Labs/15/setup.ps1
@@ -166,6 +166,16 @@ Get-ChildItem "./data/*.csv" -File | Foreach-Object {
     Write-Host $file
     $blobPath = "data/$file"
     Set-AzStorageBlobContent -File $_.FullName -Container "files" -Blob $blobPath -Context $storageContext
+
+# Load data
+write-host "Loading data..."
+Get-ChildItem "./data/*.txt" -File | Foreach-Object {
+    write-host ""
+    $file = $_.FullName
+    Write-Host "$file"
+    $table = $_.Name.Replace(".txt","")
+    bcp dbo.$table in $file -S "$synapseWorkspace.sql.azuresynapse.net" -U $sqlUser -P $sqlPassword -d $sqlDatabaseName -f $file.Replace("txt", "fmt") -q -k -E -b 5000
+}
 }
 
 
